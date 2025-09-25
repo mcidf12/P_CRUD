845b8f671845b7a7c728299292cedb0ab5dc0dd3
mcidf12 <mcid653@gmail.com>
2025-09-25 14:18:47 -0600

    recordatorio diario

--- app/Console/Commands/EnviarRecordatorios.php
+++ app/Console/Commands/EnviarRecordatorios.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Carbon\Carbon;
 use App\Models\Agenda;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
 
 class EnviarRecordatorios extends Command
 {
@@ -26,15 +27,27 @@ class EnviarRecordatorios extends Command
      *
      * @return int
      */
-    public function handle() :int
+    public function handle() 
     {
         $today = Carbon::today()->toDateString();
+        $now = Carbon::now();
+        //echo $now->toDateString();
+        //echo $now->toTimeString();
 
         $agendas=Agenda::whereDate('fechaInicio', $today)->get();
 
         foreach ($agendas as $agenda) {
-            $this->info("Recordatorio: {$agenda->tipo} - {$agenda->descripcion} ({$agenda->horaInicio})");
+            $mensaje="Recordatorio: {$agenda->tipo} - {$agenda->descripcion} ({$agenda->horaInicio})";
+            $this->info($mensaje);
+
+            Log::info($mensaje, [
+                "Tipo"=> $agenda->tipo,
+                "Descripcion"=> $agenda->descripcion,
+                "Municipio"=> $agenda->municipio,
+                "Fecha de Inicio"=> $agenda->fechaInicio,
+                "Hora de Inicio"=> $agenda->horaInicio,
+            ]);
         }
-        return Command::SUCCESS;
+        //return Command::SUCCESS;
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-         $schedule->command('recordatorios:enviar')->daily('');
+         $schedule->command('recordatorios:enviar')->everyMinute();
     }
 
     /**
